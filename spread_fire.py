891d8e2cdb328f823d2a24fa7d0c79721c40414d
shellyper <shellyper83@gmail.com>
2025-01-27 13:42:37 +0200

    Update spread_fire.py

--- spread_fire.py
+++ spread_fire.py
@@ -1,16 +1,52 @@
 import copy
-
-
 def spread_fire(grid):
+
     """Update the forest grid based on fire spreading rules."""
-    grid_size = len(grid)
     update_grid = copy.deepcopy(grid)
-    for i in range(grid_size-1):
-        for j in range(grid_size-1):
-            if grid[i][j] == 1:
+    for i in range(grid_size):
+        for j in range(grid_size):
+          if i == 0 and j == 0:
+            neighbors = [grid[i+1][j],grid[i][j+1]]
+            if 2 in neighbors:
+                update_grid[i][j] = 2
+
+          elif i == 0 and j == len(grid) - 1 :
+            neighbors = [grid[i+1][j],grid[i][j-1]]
+            if 2 in neighbors:
+                update_grid[i][j] = 2
+
+          elif i == len(grid) - 1  and j == 0:
+            neighbors = [grid[i-1][j],grid[i][j+1]]
+            if 2 in neighbors:
+                update_grid[i][j] = 2
+
+          elif i == len(grid) - 1 and j == len(grid) - 1:
+            neighbors = [grid[i-1][j],grid[i][j-1]]
+            if 2 in neighbors:
+                update_grid[i][j] = 2
+
+          elif i == 0:
+              neighbors = [grid[i+1][j],grid[i][j-1],grid[i][j+1]]
+              if 2 in neighbors:
+                  update_grid[i][j] = 2
+
+          elif i == len(grid) - 1:
+              neighbors = [grid[i-1][j],grid[i][j-1],grid[i][j+1]]
+              if 2 in neighbors:
+                  update_grid[i][j] = 2
+
+          elif j == 0:
+              neighbors = [grid[i-1][j],grid[i+1][j],grid[i][j+1]]
+              if 2 in neighbors:
+                  update_grid[i][j] = 2
+
+          elif j == len(grid) - 1:
+              neighbors = [grid[i-1][j],grid[i+1][j],grid[i][j-1]]
+              if 2 in neighbors:
+                  update_grid[i][j] = 2
+
+          elif grid[i][j] == 1:
                 neighbors = [grid[i-1][j],grid[i+1][j],grid[i][j-1],grid[i][j+1]]
                 if 2 in neighbors:
                     update_grid[i][j] = 2
-
     return update_grid
-
